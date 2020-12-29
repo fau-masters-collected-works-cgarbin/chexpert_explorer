624d6171448a8da443ab6a317a83121beb889316
Christian Garbin <garbin.christian@gmail.com>
2020-12-29 15:09:54 -0500

    Add assertions to check that transformations are correct

--- chexpert_latex_export.py
+++ chexpert_latex_export.py
@@ -17,6 +17,7 @@ DIR_TABLES = os.path.join('..', 'chexpert-datasheet', 'tables') + os.sep
 DIR_GRAPHS = os.path.join('..', 'chexpert-datasheet', 'graphs') + os.sep
 
 IMAGES = 'Images'
+PATIENTS = 'Patients'
 FLOAT_FORMAT = '{:0,.1f}'.format
 INT_FORMAT = '{:,}'.format
 
@@ -31,6 +32,9 @@ caption = 'Number of patients and images'
 stats = df.groupby([cd.COL_TRAIN_VALIDATION], as_index=True, observed=True).agg(
     Patients=(cd.COL_PATIENT_ID, pd.Series.nunique),
     Images=(cd.COL_VIEW_NUMBER, 'count'))
+assert stats.loc[cd.TRAINING][PATIENTS].sum() == cd.PATIENT_NUM_TRAINING
+assert stats[PATIENTS].sum() == cd.PATIENT_NUM_TOTAL
+assert stats[IMAGES].sum() == cd.IMAGE_NUM_TOTAL
 stats.to_latex(buf=DIR_TABLES+name+'.tex',
                formatters=[INT_FORMAT] * stats.shape[1],
                float_format=FLOAT_FORMAT, index_names=False,
@@ -41,12 +45,11 @@ name = 'patient-images-stats-summary'
 caption = 'Summary statistics for images per patient'
 stats = df.groupby([cd.COL_TRAIN_VALIDATION, cd.COL_PATIENT_ID], as_index=True, observed=True).agg(
     Images=(cd.COL_VIEW_NUMBER, 'count'))
+assert stats.loc[cd.TRAINING][IMAGES].sum() == cd.IMAGE_NUM_TRAINING
+assert stats[IMAGES].sum() == cd.IMAGE_NUM_TOTAL
 summary = stats.groupby([cd.COL_TRAIN_VALIDATION], as_index=True).agg(
-    Min=('Images', 'min'),
-    Max=('Images', 'max'),
-    Mean=('Images', 'mean'),
-    Median=('Images', 'median'),
-    Std=('Images', 'std'))
+    Min=(IMAGES, 'min'), Max=(IMAGES, 'max'), Mean=(IMAGES, 'mean'), Median=(IMAGES, 'median'),
+    Std=(IMAGES, 'std'))
 summary.to_latex(buf=DIR_TABLES+name+'.tex',
                  float_format=FLOAT_FORMAT, index_names=False,
                  caption=caption, label='tab:' + name, position='h!')
@@ -61,8 +64,9 @@ stats[IMAGE_SUMMARY] = pd.cut(stats.Images, bins=bins, labels=bin_labels, right=
 
 summary = stats.reset_index().groupby([IMAGE_SUMMARY], as_index=True, observed=True).agg(
     Patients=(cd.COL_PATIENT_ID, pd.Series.nunique))
-print(summary.columns)
+assert summary[PATIENTS].sum() == cd.PATIENT_NUM_TOTAL
 summary.to_latex(buf=DIR_TABLES+name+'.tex',
                  formatters=[INT_FORMAT] * summary.shape[1],
                  float_format=FLOAT_FORMAT, index_names=True,
                  caption=caption, label='tab:' + name, position='h!')
+
